e007fafed2323704496818d7aa7f9b3aceaf19c7
Yury <yuryloguinov@192.168.1.13>
2025-02-22 19:21:00 +0100

    Test deploy - delete if something wrong

--- components/GameTable.tsx
+++ components/GameTable.tsx
@@ -137,17 +137,18 @@ export default function GameTable({
         {/* Cards on table */}
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex space-x-2">
           {cardsOnTable.map((card, index) => (
-            <PlayingCard
+            <div
               key={index}
-              suit={card.suit}
-              value={card.value}
-              disabled
-              isHighest={
-                gameData.highestCard !== undefined &&
+              className={
+                gameData.highestCard &&
                 card.suit === gameData.highestCard.suit &&
                 card.value === gameData.highestCard.value
+                  ? "ring-4 ring-yellow-400 rounded-lg"
+                  : ""
               }
-            />
+            >
+              <PlayingCard suit={card.suit} value={card.value} disabled />
+            </div>
           ))}
         </div>
       </div>
--- types/game.ts
+++ types/game.ts
@@ -1,39 +1,38 @@
 export interface Player {
-    name: string
-    seatNumber: number
-    isOwner: boolean
-    hand: Card[]
-    score: number
-    roundWins: number
-  }
-  
-  export interface Card {
-    suit: "spades" | "hearts" | "diamonds" | "clubs"
-    value: number // 6-14, where 11=J, 12=Q, 13=K, 14=A
-    playerName?: string
-  }
-  
-  export interface ScoreTableRow {
-    roundId: number
-    roundName: string
-    scores: { [playerName: string]: number | null }
-  }
-  
-  export interface GameData {
-    tableId: string
-    players: Player[]
-    gameStarted: boolean
-    currentRound: number
-    currentPlay: number
-    currentTurn: number
-    cardsOnTable: Card[]
-    deck: Card[]
-    scoreTable: ScoreTableRow[]
-    allCardsPlayedTimestamp: number | null
-    playEndTimestamp: number | null
-    lastPlayedCard: Card | null
-    allCardsPlayed: boolean // Add this line
-    highestCard: Card | undefined
-  }
-  
-  
+  name: string
+  seatNumber: number
+  isOwner: boolean
+  hand: Card[]
+  score: number
+  roundWins: number
+}
+
+export interface Card {
+  suit: "spades" | "hearts" | "diamonds" | "clubs"
+  value: number // 6-14, where 11=J, 12=Q, 13=K, 14=A
+  playerName?: string
+}
+
+export interface ScoreTableRow {
+  roundId: number
+  roundName: string
+  scores: { [playerName: string]: number | null }
+}
+
+export interface GameData {
+  tableId: string
+  players: Player[]
+  gameStarted: boolean
+  currentRound: number
+  currentPlay: number
+  currentTurn: number
+  cardsOnTable: Card[]
+  deck: Card[]
+  scoreTable: ScoreTableRow[]
+  allCardsPlayedTimestamp: number | null
+  playEndTimestamp: number | null
+  lastPlayedCard: Card | null
+  allCardsPlayed: boolean // Add this line
+  highestCard: Card | null
+}
+
