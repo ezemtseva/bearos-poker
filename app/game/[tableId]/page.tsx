bc3d89b1a78d8d72de1a221d228333a085038ec3
Yury <yuryloguinov@192.168.1.13>
2025-07-06 09:53:34 +0200

    Sound updates

--- app/game/[tableId]/page.tsx
+++ app/game/[tableId]/page.tsx
@@ -74,7 +74,7 @@ export default function Game() {
     fetchGameState()
 
     // Set up polling
-    pollingIntervalRef.current = setInterval(fetchGameState, 2500)
+    pollingIntervalRef.current = setInterval(fetchGameState, 2000)
 
     return () => {
       if (pollingIntervalRef.current) {
